473f092ba6bab5159a8d40481a0d9a34472326ac
AhmerHub <muhammadahmernadeem@gmail.com>
2024-09-17 11:20:43 +0500

    delete workout

--- frontend/src/components/WorkoutDetails.js
+++ frontend/src/components/WorkoutDetails.js
@@ -1,21 +1,27 @@
-// const WorkoutDetails = ( workout ) => {
-//     return(<div className="workout-details">
-//         <h4>{workout.title}</h4>
-//         <p><strong>Load (kg): </strong>{workout.load}</p>
-//         <p><strong>Reps :</strong>{workout.reps}</p>
-//         <p>{workout.createdAt}</p>
-//     </div>)
-// }
-
-
+import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 // export default WorkoutDetails
 const WorkoutDetails = ({ workout }) => {  // Destructure the workout prop
+    const { dispatch } = useWorkoutsContext()
+
+    const handleClick = async() => {
+        const response = await fetch('/api/workouts/' + workout._id, {
+            method: 'DELETE'
+        })
+
+        const json = await response.json()
+
+        if(response.ok) {
+            dispatch({type: 'DELETE_WORKOUT', payload: json})
+        }
+    }
+
     return (
         <div className="workout-details">
             <h4>{workout.title}</h4>
             <p><strong>Load (kg): </strong>{workout.load}</p>
             <p><strong>Reps :</strong>{workout.reps}</p>
             <p>{new Date(workout.createdAt).toLocaleString()}</p> {/* Optional: Formatting date */}
+            <span onClick={handleClick}>Delete</span>
         </div>
     )
 }
